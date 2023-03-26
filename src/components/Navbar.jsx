accc72b78af73443c5cc4c07d88c75814ba1a110
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2023-03-26 17:11:42 -0300

    React Hooks and Navbar
    I've added the react hooks, with one hook for the home, movie, and search pages. Additionally, I've changed the navbar by adding a search bar and making it more mobile-friendly.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { MdMovie, MdClose } from 'react-icons/md';
+import { MdMovie, MdClose, MdOutlineSearch } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -35,7 +36,7 @@ const Navbar = () => {
   return (
     <div className="bg-mtblack fixed top-0 w-full z-50">
       <div className="flex justify-between items-center h-20 max-w-[1200px] mx-auto px-6">
-        <h1 className="w-full text-5xl font-bold text-mtyellow title-font">
+        <h1 className="w-full md:text-6xl text-4xl font-bold text-mtyellow title-font mt-2">
           <a href="#home">Movie Twins</a>
         </h1>
         <ul className="md:flex hidden justify-center items-center">
@@ -45,35 +46,55 @@ const Navbar = () => {
           <li className="px-4 py-2 text-2xl text-mtpurple hover:underline">
             <a href="#menu">TMDB</a>
           </li>
-          <li className="flex justify-center items-center pl-4">
-            <a href="#contact" className=" bg-mtpurple text-mtgray px-4 py-2 text-2xl rounded-lg">
-              Contact
-            </a>
+          <li>
+            <div className="flex items-center ml-4">
+              <div className="relative">
+              <input
+                type="text"
+                placeholder="Search for a twin"
+                className="lg:w-96 w-64 py-1 px-2 text-xl rounded-md focus:outline-none focus:shadow-outline bg-mtgray text-mtwhite"
+              />
+                <button type="submit" className="absolute inset-y-0 right-0 flex rounded-md items-center px-4  bg-mtgray">
+                  <MdOutlineSearch size={26} className="text-mtpurple hover:text-mtyellow hover:scale-125 duration-300" />
+                </button>
+              </div>
+            </div>
           </li>
         </ul>
-        <div className="text-mtpurple block md:hidden" onClick={handleNav}>
-          {!nav ? <MdMovie size={30} /> : <MdClose size={36} />}
+        <div className="text-mtpurple bg-mtgray p-2 rounded-full block md:hidden z-50" onClick={handleNav}>
+          {!nav ? <MdMovie size={28}/> : <MdClose size={28}/>}
         </div>
-        <div className=" z-50">
+        <div className=" z-20">
           <div
             className={
               !nav
-                ? 'ease-in-out duration-100 fixed left-[-100%]'
-                : 'fixed left-0 top-0 w-[60%] h-full border-r border-r-mtblack bg-mtgray ease-in-out duration-500'
+                ? 'ease-in-out fixed top-[-40%]'
+                : 'fixed left-0 top-0 w-full border-b border-b-mtyellow bg-mtblack ease-in-out duration-500 z-20'
             }
           >
-            <h1 className="w-full text-4xl font-bold text-mtyellow m-4 title-font">
+            <h1 className="w-full text-4xl font-bold text-mtyellow m-6 title-font">
               Movie Twins
             </h1>
             <ul className="text-mtpurple">
-              <li className="p-4 text-2xl border-b border-mtblack">
-                <a href="#home">Home</a>
+              <li className='border-b border-mtyellow'>
+                <div className="p-4 flex items-center justify-center mb-3">
+                  <div className="relative">
+                  <input
+                    type="text"
+                    placeholder="Search for a twin"
+                    className="w-[100%] py-1 px-2 text-xl rounded-md focus:outline-none focus:shadow-outline bg-mtgray text-mtwhite"
+                  />
+                    <button type="submit" className="absolute inset-y-0 right-0 flex rounded-md items-center px-4  bg-mtgray">
+                      <MdOutlineSearch size={26} className="text-mtpurple hover:text-mtyellow hover:scale-125 duration-300" />
+                    </button>
+                  </div>
+                </div>
               </li>
-              <li className="p-4 text-2xl border-b border-mtblack">
-                <a href="#menu">TMDB</a>
+              <li className="p-6 text-2xl border-b border-mtyellow flex items-center justify-center">
+                <a href="#home" className='hover:underline'>Home</a>
               </li>
-              <li className="p-4 text-2xl border-b border-mtblack">
-                <a href="#contact">Contact</a>
+              <li className="p-6 text-2xl border-b border-mtyellow flex items-center justify-center">
+                <a href="#menu" className='hover:underline'>TMDB</a>
               </li>
             </ul>
           </div>
