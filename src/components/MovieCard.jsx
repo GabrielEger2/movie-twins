375097f9a34cdce56db495046c06abc8f5acacab
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2023-04-03 15:37:10 -0300

    Movie card and Search route stylised
    I've added style to the Search page and the movie card component, making them show in columns

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { BsFillStarFill } from 'react-icons/bs'
+import { Link } from 'react-router-dom';
+import { BsFillStarFill } from 'react-icons/bs';
 
-const imageUrl = 'https://image.tmdb.org/t/p/w500/'
+const imageUrl = 'https://image.tmdb.org/t/p/w500/';
 
 const MovieCard = ({ movie, showLink = true }) => {
   return (
-    <div>
-        <img src={imageUrl + movie.poster_path} alt={movie.title} />
-        <h2>{movie.title}</h2>
-        <p>
-            <BsFillStarFill className='text-yellow-400' /> {movie.vore_average}
+    <div className='max-w-[1640px] m-auto'>
+      <img src={imageUrl + movie.poster_path} alt={movie.title} className='w-full object-cover rounded-t-lg' />
+      <div className='flex justify-between px-2 mt-2 flex-col'>
+        <h2 className=' text-center text-mtwhite text-xl md:text-2xl lg:text-3xl font-bold'>{movie.title}</h2>
+        <p className=' flex-row flex justify-center gap-2 mt-2 text-mtyellow content-center text-2xl'>
+          <BsFillStarFill size={26} className="text-yellow-400" /> {movie.vote_average}
         </p>
-        {showLink && <Link to={`/movie/${movie.id}`}></Link>}
+        <div className='flex justify-center'>
+          <div style={{ height: '80px', display: 'flex', alignItems: 'center' }}>
+            <button className='mt-4 mb-6 bg-mtyellow text-mtgray border-mtblack rounded-none py-4 px-8 title-font text-2xl hover:scale-110 duration-300 tracking-[0.1rem] font-bold justify-center items-end'>
+              {showLink && <Link to={`/movie/${movie.id}`}>View Details</Link>}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
--- src/routes/Search.jsx
+++ src/routes/Search.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import PageNotFound from "./PageNotFound";
+import SearchBar from '../components/SearchBar';
 
 const searchUrl = 'https://api.themoviedb.org/3/search/movie';
 const apiKey = import.meta.env.VITE_TMDB_KEY;
@@ -12,6 +13,7 @@ const Search = () => {
   const [foundMovies, setFoundMovies] = useState(true);
 
   const [movies, setMovies] = useState([]);
+  const [movieTitle, setMovieTitle] = useState("");
   const query = new URLSearchParams(location.search).get('search');
   const encodedQuery = encodeURIComponent(query);
 
@@ -21,13 +23,14 @@ const Search = () => {
       const data = await res.json();
       if (res.status === 200 && data.results.length > 0) {
         const movieId = data.results[0].id;
+        const movieTitle = data.results[0].title;
         const recommendationsUrl = `https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=${apiKey}`;
+        setMovieTitle(movieTitle);
         getRecommendations(recommendationsUrl);
       } else {
         setFoundMovies(false);
       }
     } catch (error) {
-      console.log(error);
       setFoundMovies(false);
     }
   };
@@ -44,13 +47,32 @@ const Search = () => {
   }, [encodedQuery]);
 
   return (
-    <div className='mt-20'>
+    <div className='mt-20 bg-[#413543]'>
       {foundMovies ? (
         <>
-          <h1>Search for {query}</h1>
-          {movies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} />
-          ))}
+          <div>
+            <div className='pt-14 text-mtwhite font-bold text-5xl lg:text-6xl text-center title-font tracking-[0.2rem] text-shadow'>
+              <h1 className='pb-2 lg:pb-4'>
+                Showing similar movies to:
+              </h1>
+              <h1 className='text-mtyellow tracking-[0.2rem]'
+                style={{ textShadow: '4px 4px #8F43EE, 4px 4px 12px black'}}>
+                {movieTitle}
+              </h1>
+            </div>
+            <div className="max-w-[1640px] m-auto px-4">
+              <div className='flex flex-col lg:flex-row justify-between py-8'>
+                <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+                  {movies.map((movie) => (
+                    <div className='border border-mtyellow shadow-lg bg-mtdarkgray rounded-lg'>
+                      <MovieCard key={movie.id} movie={movie} />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <SearchBar />
+          </div>
         </>
       ) : (
         <PageNotFound />
