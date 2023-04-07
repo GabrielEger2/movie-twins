bd1615112a3c3b6dfa5e37054993721abd2d3a2a
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2023-04-07 17:21:07 -0300

    dotenv added
    I've added the dotenv file as a response to the vercel problems

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -27,7 +27,7 @@ const MovieCard = ({ movie, showLink = true }) => {
       <img src={imageUrl + movie.poster_path} alt={movie.title} className='w-full object-cover rounded-t-lg' />
       <div className='flex justify-between px-2 mt-2 flex-col'>
         <h2 className=' text-center text-mtwhite text-xl md:text-2xl lg:text-3xl font-bold'>{movie.title}</h2>
-        <p className=' flex-row flex justify-center gap-2 mt-2 text-mtyellow content-center text-2xl'>
+        <p className=' flex-row flex justify-center gap-2 my-2 text-mtyellow content-center text-2xl'>
           <BsFillStarFill size={26} className="text-yellow-400" /> {movie.vote_average}
         </p>
       </div>
--- src/routes/Movie.jsx
+++ src/routes/Movie.jsx
@@ -20,7 +20,6 @@ const Movie = () => {
   const getMovie = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
     setMovie(data);
   };
 
