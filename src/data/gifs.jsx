5cd2dfe4ab3f655e5b7cb44cbe1c925bf10d73d4
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2023-04-07 17:57:09 -0300

    New gifs added
    I've added new gifs and changed the topRatedUrl' api request path

--- src/data/gifs.jsx
+++ src/data/gifs.jsx
@@ -5,5 +5,9 @@ export const gifs = [
     "https://thumbs.gfycat.com/LavishGracefulAmphibian-max-1mb.gif",
     "https://i0.wp.com/media4.giphy.com/media/bAUp9Lcx1HY4/giphy.gif",
     "https://giffiles.alphacoders.com/216/216074.gif",
-    "https://imgs.search.brave.com/Ka0YQZ-cwl7YNTc1LAOdyT4K-GV1AlqyTrYmOg-UME4/rs:fit:500:299:1/g:ce/aHR0cDovLzMuYnAu/YmxvZ3Nwb3QuY29t/Ly1Xc0hXRjZsVEMx/Zy9UbGdIcGVkOVdw/SS9BQUFBQUFBQU1x/WS9KSVllWnhqWDB3/Zy9zNjQwL2F3ZXNv/bWVfY2xhc3NpY19t/b3ZpZV9naWZzXzAy/LmdpZg.gif"
+    "https://imgs.search.brave.com/Ka0YQZ-cwl7YNTc1LAOdyT4K-GV1AlqyTrYmOg-UME4/rs:fit:500:299:1/g:ce/aHR0cDovLzMuYnAu/YmxvZ3Nwb3QuY29t/Ly1Xc0hXRjZsVEMx/Zy9UbGdIcGVkOVdw/SS9BQUFBQUFBQU1x/WS9KSVllWnhqWDB3/Zy9zNjQwL2F3ZXNv/bWVfY2xhc3NpY19t/b3ZpZV9naWZzXzAy/LmdpZg.gif",
+    "https://imgs.search.brave.com/u9TXEeECYNLdePB80FGDOodkrJ7laRYL3PIToW7Fuo0/rs:fit:500:225:1/g:ce/aHR0cDovLzEuYnAu/YmxvZ3Nwb3QuY29t/Ly1DSFFCUEhudnM3/MC9UbGdJWFBtNGt3/SS9BQUFBQUFBQU1y/ay95ZEtGSTVuMXFx/cy9zNjQwL2F3ZXNv/bWVfY2xhc3NpY19t/b3ZpZV9naWZzXzIz/LmdpZg.gif",
+    "https://imgs.search.brave.com/6tf3C_KC9nW1dkejab_T9RXP3SjiHeQNt2InI5AqRx8/rs:fit:450:254:1/g:ce/aHR0cHM6Ly9tZWRp/YS5naXBoeS5jb20v/bWVkaWEvckNtQzEy/T1d6OWtUUy9naXBo/eS5naWY.gif",
+    "https://imgs.search.brave.com/zTeCXgwPvCeNIVSqKWiDu1CwlHvCiXDu1y7AyA-dGzA/rs:fit:480:248:1/g:ce/aHR0cHM6Ly9tZWRp/YTQuZ2lwaHkuY29t/L21lZGlhLzNvNlp0/NmN6UVh3UWhRakhR/US9naXBoeS5naWY.gif",
+    "https://imgs.search.brave.com/iy1mMW4TrWGTZ92JGEJBQ4wGXfFNdDe2WxDoxZHUeus/rs:fit:500:234:1/g:ce/aHR0cHM6Ly8yNC5t/ZWRpYS50dW1ibHIu/Y29tLzNhYmNlY2Fj/ZDY5ZmQ0YzM1Yjc3/M2U1MDE2MjQ1NDg1/L3R1bWJscl9tbjF0/ZTRDN0ZBMXNyeTVr/MW8xXzUwMC5naWY.gif"
   ];
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -15,8 +15,7 @@ const Home = () => {
   };
 
   useEffect(() => {
-    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
-    console.log(topRatedUrl);
+    const topRatedUrl = `${moviesURL}top_rated?api_key=${apiKey}`;
     getTopRatedMovies(topRatedUrl);
   }, []);
 
